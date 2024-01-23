09ac64292e46608374b387bb5ff2d1ee2056968f
Raushan <roushankumarsingh2018@gmail.com>
2024-01-23 08:09:48 +0530

    added async

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Async from './components/Async';
 import Greeting from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
-      <Greeting />
+      {/* <Greeting /> */}
+      <Async />
     </div>
   );
 }
